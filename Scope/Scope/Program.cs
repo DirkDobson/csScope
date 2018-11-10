8422f29b2a1fdb0ad121244d824e81db4563cc92
Dirk Dobson <dirkvancouver@live.com>
2018-11-09 19:33:22 -0700

    inial commit

---
+++ Scope/Scope/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
