4144f6f88ea83180dc363c15ad15ec1da90a70fb
Dirk Dobson <dirkvancouver@live.com>
2018-11-09 19:57:47 -0700

    const

--- Scope/Scope/Program.cs
+++ Scope/Scope/Program.cs
@@ -6,6 +6,8 @@ namespace Scope
     {
         static void Main(string[] args)
         {
+            const float Pi = 3.14f;
+            Console.WriteLine(Pi);
             Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
             Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
         }
